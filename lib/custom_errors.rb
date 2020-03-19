00feb3fdee9e83f1e0e07661e73df7fe88ea53c9
Pharia Le <pharia.le@gmail.com>
2020-03-19 04:22:56 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,16 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-
+  
+  class PartnerError < StandardError
+  end
+  
 end
 
 beyonce = Person.new("Beyonce")
